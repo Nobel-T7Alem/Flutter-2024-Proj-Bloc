2e5334c9b408a08eae120aceb05d3a69b22057ce
Nobel <alemayehu.nobel.3@gmail.com>
2024-05-26 12:57:45 +0300

    Bloc Init

---
+++ lib/presentation/screens/agency_signup.dart
@@ -0,0 +1,128 @@
+import 'package:Sebawi/presentation/widgets/custom_button.dart';
+import 'package:Sebawi/presentation/widgets/custom_text_field.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class AgencySignup extends StatefulWidget {
+  const AgencySignup({super.key});
+
+  @override
+  _AgencySignupState createState() => _AgencySignupState();
+}
+
+class _AgencySignupState extends State<AgencySignup> {
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 35),
+                  child: Image.asset(
+                    'assets/images/sebawilogo.png',
+                    width: 140.0,
+                    height: 140.0,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 35),
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const SizedBox(
+                  height: 2.0,
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 35.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 8.0),
+                      Text(
+                        'Share your volunteering opportunities.',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 8.0),
+                const CustomTextFormField(
+                  labelText: 'Agency name',
+                  obscureText: true,
+                ),
+                const SizedBox(height: 10.0),
+                const CustomTextFormField(
+                  labelText: 'Enter Email',
+                  obscureText: true,
+                ),
+                const SizedBox(height: 10.0),
+                const CustomTextFormField(
+                  labelText: 'Create Username',
+                  obscureText: true,
+                ),
+                const SizedBox(height: 10.0),
+                const CustomTextFormField(
+                  labelText: 'Create Password',
+                  obscureText: true,
+                ),
+                const SizedBox(height: 10.0),
+                const CustomTextFormField(
+                  labelText: 'Confirm Password',
+                  obscureText: true,
+                ),
+                const SizedBox(height: 40.0),
+                CustomButton(
+                    buttonText: 'Signup',
+                    buttonColor: const Color.fromARGB(255, 83, 171, 71),
+                    buttonTextColor: Colors.white,
+                    buttonAction: () {
+                      context.go('/agency_home');
+                    }),
+                Padding(
+                  padding: const EdgeInsets.all(17),
+                  child: Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(right: 4.0),
+                          child: Text('Already signed up?'),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            context.go('/user_login');
+                          },
+                          child: const Text(
+                            'Login',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(255, 66, 148, 69),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
