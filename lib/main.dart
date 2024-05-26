0d4edfdf604d393f756d92c880ae065342fa87f5
Nobel <alemayehu.nobel.3@gmail.com>
2024-05-26 16:17:35 +0300

    Bloc State Management First iteration.

--- lib/main.dart
+++ lib/main.dart
@@ -1,123 +1,24 @@
-import 'package:Sebawi/presentation/screens/admin_login.dart';
-import 'package:Sebawi/presentation/screens/admin_page.dart';
-import 'package:Sebawi/presentation/screens/agency_home.dart';
-import 'package:Sebawi/presentation/screens/agency_signup.dart';
-import 'package:Sebawi/presentation/screens/agency_update.dart';
-import 'package:Sebawi/presentation/screens/login_page.dart';
-import 'package:Sebawi/presentation/screens/login_user.dart';
-import 'package:Sebawi/presentation/screens/signup_page.dart';
-import 'package:Sebawi/presentation/screens/user_update.dart';
-import 'package:Sebawi/presentation/screens/volunteer-signup.dart';
 import 'package:flutter/material.dart';
-import 'package:Sebawi/presentation/screens/home_page.dart';
-import 'package:Sebawi/presentation/screens/user_home.dart';
-import 'package:go_router/go_router.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'app_router.dart';
+import 'blocs/home/home_page_bloc.dart';
+import 'blocs/login/login_page_bloc.dart';
+import 'blocs/admin_login/admin_login_bloc.dart';
+import 'blocs/admin_page/admin_page_bloc.dart';
+import 'blocs/agency_home/agency_home_bloc.dart';
+import 'blocs/agency_signup/agency_signup_bloc.dart';
+import 'blocs/agency_update/agency_update_bloc.dart';
+import 'blocs/user_home/user_home_bloc.dart';
+import 'blocs/user_update/user_update_bloc.dart';
+import 'blocs/volunteer_signup/volunteer_signup_bloc.dart';
+import 'blocs/signup/signup_page_bloc.dart';
+import 'blocs/login_user/login_user_bloc.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-final GoRouter _router = GoRouter(
-  initialLocation: '/',
-  routes: [
-    GoRoute(
-      path: '/',
-      name: 'home',
-      builder: (context, state) => const HomePage(),
-    ),
-    GoRoute(
-      path: '/signup',
-      name: "signup",
-      builder: (context, state) => const SignupScreen(),
-    ),
-    GoRoute(
-      path: '/login',
-      name: "login",
-      builder: (context, state) => const LoginPage(),
-    ),
-    GoRoute(
-      path: '/admin',
-      name: "admin",
-      builder: (context, state) => const AdminPage(),
-    ),
-    GoRoute(
-      path: '/user_login',
-      name: "user_login",
-      builder: (context, state) => const LoginUser(),
-    ),
-    GoRoute(
-      path: '/admin_login',
-      name: "admin_login",
-      builder: (context, state) => const AdminLoginPage(),
-    ),
-    GoRoute(
-      path: '/user_home',
-      name: "user_home",
-      builder: (context, state) => const UserHomePage(),
-    ),
-    GoRoute(
-      path: '/volunteer_signup',
-      name: "volunteer_signup",
-      builder: (context, state) => VolunteerSignup(),
-    ),
-    GoRoute(
-      path: '/agency_signup',
-      name: "agency_signup",
-      builder: (context, state) => const AgencySignup(),
-    ),
-    GoRoute(
-        path: '/agency_home',
-        name: "agency_home",
-        builder: (context, state) => const AgencyHomePage()),
-    GoRoute(
-      path: '/user_update',
-      name: "user_update",
-      builder: (context, state) => const UserUpdate(),
-    ),
-    GoRoute(
-      path: '/agency_update',
-      name: "agency_update",
-      builder: (context, state) => const AgencyUpdate(),
-    ),
-    GoRoute(
-      path: '/admin_page',
-      name: "admin_page",
-      builder: (context, state) => const AdminPage(),
-    ),
-  ],
-  errorBuilder: (context, state) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Center(
-                child: Text(
-              'Error: ${state.error}',
-              style: const TextStyle(fontSize: 20),
-              textAlign: TextAlign.center,
-            )),
-            ElevatedButton(
-              onPressed: () {
-                context.go('/');
-              },
-              child: const Text(
-                'Return to Home Page',
-                style: TextStyle(color: Colors.black),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  },
-);
-
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
