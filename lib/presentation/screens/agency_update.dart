cf87d10ff9712504a58e6742d95b6f0886f97622
Nobel <alemayehu.nobel.3@gmail.com>
2024-05-28 19:17:43 +0300

    overwrites

--- lib/presentation/screens/agency_update.dart
+++ lib/presentation/screens/agency_update.dart
@@ -1,17 +1,34 @@
 import 'package:Sebawi/presentation/widgets/profile_update_form.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:Sebawi/blocs/agency_update/agency_update_event.dart';
+import 'package:Sebawi/blocs/agency_update/agency_update_bloc.dart';
+import 'package:Sebawi/blocs/agency_update/agency_update_state.dart';
+import 'package:go_router/go_router.dart';
 
 class AgencyUpdate extends StatelessWidget {
   const AgencyUpdate({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar:AppBar(
-        title: const Text('Update Profile'),
-      ) ,
-      body: const Padding(padding: EdgeInsets.all(16.0),
-      child: ProfileUpdateForm(),),
+    return BlocProvider(
+      create: (context) => AgencyUpdateBloc(),
+      child: BlocListener<AgencyUpdateBloc, AgencyUpdateState>(
+        listener: (context, state) {
+          if (state is AgencyUpdateNavigationSuccess) {
+            context.go(state.route);
+          }
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Update Profile'),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ProfileUpdateForm(),
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/presentation/screens/volunteer_signup.dart
+++ lib/presentation/screens/volunteer_signup.dart
@@ -1,125 +1,140 @@
 import 'package:Sebawi/presentation/widgets/custom_button.dart';
 import 'package:Sebawi/presentation/widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:Sebawi/blocs/volunteer_signup/volunteer_signup_bloc.dart';
+import 'package:Sebawi/blocs/volunteer_signup/volunteer_signup_state.dart';
+import 'package:Sebawi/blocs/volunteer_signup/volunteer_signup_event.dart';
 import 'package:go_router/go_router.dart';
 
 class VolunteerSignup extends StatefulWidget {
-  const VolunteerSignup({super.key});
-
   @override
   _VolunteerSignupState createState() => _VolunteerSignupState();
 }
 
 class _VolunteerSignupState extends State<VolunteerSignup> {
-  final TextEditingController _usernameController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Container(
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 35),
-                  child: Image.asset(
-                    'assets/images/sebawilogo.png',
-                    width: 140.0,
-                    height: 140.0,
-                  ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(left: 35),
-                  child: Text(
-                    'Sign Up',
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 35,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-                const SizedBox(
-                  height: 2.0,
-                ),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 35.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(height: 8.0),
-                      Text(
-                        'Do something good today.',
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
+    return BlocProvider(
+      create: (context) => VolunteerSignupBloc(),
+      child: BlocListener<VolunteerSignupBloc, VolunteerSignupState>(
+        listener: (context, state) {
+          if (state is NavigateToUserHome) {
+            context.go('/user_home');
+          } else if (state is NavigateToUserLogin) {
+            context.go('/user_login');
+          }
+        },
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: Scaffold(
+            body: Container(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 35),
+                      child: Image.asset(
+                        'assets/images/sebawilogo.png',
+                        width: 140.0,
+                        height: 140.0,
                       ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 8.0),
-                const CustomTextFormField(
-                  labelText: 'Full name',
-                  obscureText: true,
-                ),
-                const SizedBox(height: 10.0),
-                const CustomTextFormField(
-                  labelText: 'Enter Email',
-                  obscureText: true,
-                ),
-                const SizedBox(height: 10.0),
-                const CustomTextFormField(
-                  labelText: 'Create Username',
-                  obscureText: true,
-                ),
-                const SizedBox(height: 10.0),
-                const CustomTextFormField(
-                  labelText: 'Create Password',
-                  obscureText: true,
-                ),
-                const SizedBox(height: 10.0),
-                const CustomTextFormField(
-                  labelText: 'Confirm Password',
-                  obscureText: true,
-                ),
-                const SizedBox(height: 40.0),
-                CustomButton(
-                    buttonText: 'Sign up',
-                    buttonColor: const Color.fromARGB(255, 83, 171, 71),
-                    buttonTextColor: Colors.white,
-                    buttonAction: () {
-                      context.go('/user_home');
-                    }),
-                Padding(
-                  padding: const EdgeInsets.all(17),
-                  child: Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                           const Text('Already signed up?'),
-                        TextButton(
-                          onPressed: () {
-                            context.go('/user_login');
-                          },
-                          style: ButtonStyle(
-                            foregroundColor: WidgetStateProperty.all<Color>(
-                                const Color.fromARGB(255, 66, 148, 69)),
-                            textStyle: WidgetStateProperty.all<TextStyle>(
-                              const TextStyle(
-                                fontSize: 15,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                          ),
-                          child: const Text('Log In'),
-                        ),
-                      ],
                     ),
-                  ),
-                )
-              ],
+                    const Padding(
+                      padding: EdgeInsets.only(left: 35),
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 2.0,
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 35.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(height: 8.0),
+                          Text(
+                            'Do something good today.',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 8.0),
+                    const CustomTextFormField(
+                      labelText: 'Full name',
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 10.0),
+                    const CustomTextFormField(
+                      labelText: 'Enter Email',
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 10.0),
+                    const CustomTextFormField(
+                      labelText: 'Create Username',
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 10.0),
+                    const CustomTextFormField(
+                      labelText: 'Create Password',
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 10.0),
+                    const CustomTextFormField(
+                      labelText: 'Confirm Password',
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 40.0),
+                    CustomButton(
+                      buttonText: 'Sign up',
+                      buttonColor: const Color.fromARGB(255, 83, 171, 71),
+                      buttonTextColor: Colors.white,
+                      buttonAction: () {
+                        context.read<VolunteerSignupBloc>().add(SignupButtonPressed());
+                      },
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(17),
+                      child: Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text('Already signed up?'),
+                            TextButton(
+                              onPressed: () {
+                                context.read<VolunteerSignupBloc>().add(LoginButtonPressed());
+                              },
+                              style: ButtonStyle(
+                                foregroundColor: WidgetStateProperty.all<Color>(
+                                    const Color.fromARGB(255, 66, 148, 69)),
+                                textStyle: WidgetStateProperty.all<TextStyle>(
+                                  const TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                              child: const Text('Log In'),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ),
           ),
         ),
